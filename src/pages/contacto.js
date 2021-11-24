04f80ba2a0365b52644ca3fe3e5de8e0c5b5d6a3
ReactJeronimo <jerorobalino@gmail.com>
2021-11-24 17:42:37 -0500

    m

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -4,14 +4,24 @@ import Facebook from "../img/social/facebook.svg";
 import Instagram from "../img/social/instagram.svg";
 import Github from "../img/social/githubSocial.svg";
 import Linkedin from "../img/social/linkedin.svg";
+import ReactTooltip from "react-tooltip";
 
 export const Contacto = () => {
   return (
     <div>
+      <ReactTooltip
+        place="top"
+        type="dark"
+        effect="solid"
+        border={true}
+        borderColor="#383838"
+        className="tooltip-habilidades"
+      />
       <div className="sider-social">
         <div className="grid-social">
           <a
             href="https://www.facebook.com"
+            data-tip="Facebook"
             rel="noreferrer"
             target="_blank"
           >
@@ -20,6 +30,7 @@ export const Contacto = () => {
 
           <a
             href="https://www.instagram.com"
+            data-tip="Instagram"
             rel="noreferrer"
             target="_blank"
           >
@@ -28,6 +39,7 @@ export const Contacto = () => {
 
           <a
             href="https://www.github.com"
+            data-tip="Github"
             rel="noreferrer"
             target="_blank"
           >
@@ -35,6 +47,7 @@ export const Contacto = () => {
           </a>
           <a
             href="https://www.linkedin.com"
+            data-tip="Linkedin"
             rel="noreferrer"
             target="_blank"
           >
@@ -48,13 +61,15 @@ export const Contacto = () => {
           de entrada siempre está abierta. Ya sea que tenga una pregunta o
           simplemente quiera saludar, ¡haré todo lo posible para responderle!
         </p>
-        <a
-          href="mailto:gerorobalino@gmail.com"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <Btn content="Contáctame" class="contacto" />
-        </a>
+        <div className="grid-contacto">
+          <a
+            href="mailto:gerorobalino@gmail.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Btn content="Contáctame" class="contacto" />
+          </a>
+        </div>
       </div>
     </div>
   );
